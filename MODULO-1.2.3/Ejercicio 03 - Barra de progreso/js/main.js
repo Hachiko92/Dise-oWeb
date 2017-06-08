251051531bbe31535098bd07dea25dbe78464ad2
Hachiko92 <eleonora.franchini@tajamar365.com>
2017-06-08 21:40:44 +0200

    ejercicio

--- MODULO-1.2.3/Ejercicio 03 - Barra de progreso/js/main.js
+++ MODULO-1.2.3/Ejercicio 03 - Barra de progreso/js/main.js
@@ -1,23 +1,27 @@
-function progresar (){
+function progresar (tiempo){
     let progreso = document.querySelector("progress");
     let btnStop = document.getElementById("btnStop");
+    progreso.value = 0;
     btnStop.addEventListener("click", function(){
         clearInterval(refreshInterval);
     });
     var refreshInterval = setInterval(function(){
         if(progreso.value >= 100){
-            progreso.value = 0;
+            clearInterval(refreshInterval);
         }
         else{
             progreso.value += 10;
         }
-    }, 500);
+    }, tiempo);
+    return;
 }
 
 function esperar (){
     
     let btnStart = document.getElementById("btnStart");
-    btnStart.addEventListener("click", progresar);
+    btnStart.addEventListener("click", function(){
+        progresar(500);
+    });
 }
 
 $(function(){
