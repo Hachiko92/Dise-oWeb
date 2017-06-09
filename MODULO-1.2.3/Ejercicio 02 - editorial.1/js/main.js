85e13d22212237bab081176f528f1e78b65d1b07
Hachiko92 <eleonora.franchini@tajamar365.com>
2017-06-09 14:59:47 +0200

    Ejercicio

--- MODULO-1.2.3/Ejercicio 02 - editorial.1/js/main.js
+++ MODULO-1.2.3/Ejercicio 02 - editorial.1/js/main.js
@@ -1,11 +1,22 @@
-/* header che si attacca con lo scroll */
+function menu(){
+  let btnMenu = document.querySelector("#btnMenu");
+  let btnMenuImage = document.querySelector("#btnMenuImage");
+  btnMenu.addEventListener("click", desplegar);
+}
 
-$(window).scroll(function () {
-  if ( $(this).scrollTop() > 200 && !$('header').hasClass('open') ) {
-    $('header').addClass('open');
-    $('header').slideDown();
-   } else if ( $(this).scrollTop() <= 200 ) {
-    $('header').removeClass('open');
-    $('header').slideUp();
+function desplegar(){
+  let menuMobile = document.getElementById("menuMobile");
+  menuMobile.classList.toggle("oculto");
+
+  if (btnMenuImage.src.indexOf("Open.png")>-1) {
+    btnMenuImage.src = btnMenuImage.src.replace("Open.png", "Close.png");
+  } else {
+    btnMenuImage.src = btnMenuImage.src.replace("Close.png", "Open.png");
   }
-});
+}
+
+$(function (){
+  let menuTemporal = document.getElementById("menuTemporal");
+  menuTemporal.className = "oculto";
+  menu();
+})
